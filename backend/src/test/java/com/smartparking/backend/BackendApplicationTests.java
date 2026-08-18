93fdfdb77f438894475a47c1323b67f2fb2e691c
praveenchoppa <praveenkumarchoppa@gmail.com>
2026-08-18 13:11:37 +0530

    Set up Spring Boot backend

---
+++ backend/src/test/java/com/smartparking/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartparking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
